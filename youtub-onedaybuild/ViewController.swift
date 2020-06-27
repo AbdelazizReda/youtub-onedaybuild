2b88c36e46027b299e8a49a1b28fcfc5e48ae96f
abdelaziz <abdelaziz.eng@yahoo.com>
2020-06-27 13:47:01 +0200

    initial xcode project creation

---
+++ youtub-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtub-onedaybuild
+//
+//  Created by عبدالعزيز رضا  on 6/27/20.
+//  Copyright © 2020 abdelazizReda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
